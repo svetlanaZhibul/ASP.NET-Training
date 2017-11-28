ef831e8ccedded2eb6bdd3ced7285e3423f5b3bd
szhibul <nakzneags@gmail.com>
2017-11-28 09:36:10 +0300

    Task 3

--- NET.W.2017.Zhybul.Test/Task3.Console/Program.cs
+++ NET.W.2017.Zhybul.Test/Task3.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3.Solution;
 
 namespace Task3.Console
 {
@@ -10,6 +11,36 @@ namespace Task3.Console
     {
         static void Main(string[] args)
         {
+            Bank bank = new Bank();
+            Broker broker = new Broker();
+
+            Random rnd = new Random();
+            int usd = rnd.Next(20, 40);
+            int euro = rnd.Next(30, 50);
+            StockInfo info = new StockInfo();
+
+            bank.StartTrackingStockInfo(info);
+            broker.StartTrackingStockInfo(info);
+
+            info.StockInfoSender(usd, euro);
+
+            broker.StopTrackingStockInfo(info);
+
+            usd = 22;
+            euro = 50;
+
+            info.StockInfoSender(usd, euro);
+
+            System.Console.ReadKey();
+        }
+        
+
+        public void Market()
+        {
+            //Random rnd = new Random();
+            //stocksInfo.USD = rnd.Next(20, 40);
+            //stocksInfo.Euro = rnd.Next(30, 50);
+            //Notify();
         }
     }
 }
---
+++ NET.W.2017.Zhybul.Test/Task3.Solution/StockEventHandler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Solution
+{
+    public sealed class StockEventsArgs : EventArgs
+    {
+        public int USD { get; set; }
+        public int Euro { get; set; }
+
+        public StockEventsArgs() { }
+        public StockEventsArgs(int usd, int euro)
+        {
+            USD = usd;
+            Euro = euro;
+        }
+
+        //property
+    }
+
+    public class StockInfo
+    {
+        public event EventHandler<StockEventsArgs> NotifyStock;
+
+        public void StockInfoSender(int usd, int euro)
+        {
+            OnNotifyStock(new StockEventsArgs(usd, euro));
+        }
+
+        protected virtual void OnNotifyStock(StockEventsArgs args)
+        {
+            NotifyStock?.Invoke(this, args);
+        }
+    }
+
+    public class Broker
+    {
+        public Broker() { }
+
+        public string Name { get; set; }
+
+        public void StartTrackingStockInfo(StockInfo info)
+        {
+            info.NotifyStock += TrackStockInfo;
+        }
+
+        public void TrackStockInfo(object sender, StockEventsArgs stockInfo)
+        {
+            if (stockInfo.USD > 30)
+                Console.WriteLine("Брокер {0} продает доллары;  Курс доллара: {1}", this.Name, stockInfo.USD);
+            else
+                Console.WriteLine("Брокер {0} покупает доллары;  Курс доллара: {1}", this.Name, stockInfo.USD);
+
+        }
+
+        public void StopTrackingStockInfo(StockInfo info)
+        {
+            info.NotifyStock -= TrackStockInfo;
+        }
+    }
+
+    public class Bank
+    {
+        public Bank() { }
+
+        public string Name { get; set; }
+
+        public void StartTrackingStockInfo(StockInfo info)
+        {
+            info.NotifyStock += TrackStockInfo;
+        }
+
+        public void TrackStockInfo(object sender, StockEventsArgs stockInfo)
+        {
+            if (stockInfo.Euro > 40)
+                Console.WriteLine("Банк {0} продает евро;  Курс евро: {1}", this.Name, stockInfo.Euro);
+            else
+                Console.WriteLine("Банк {0} покупает евро;  Курс евро: {1}", this.Name, stockInfo.Euro);
+
+        }
+
+        public void StopTrackingStockInfo(StockInfo info)
+        {
+            info.NotifyStock -= TrackStockInfo;
+        }
+    }
+}
